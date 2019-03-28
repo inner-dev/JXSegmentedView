d6f9b58182c45c23429ce635d680d07c584cb7b0
prm <so436@126.com>
2019-03-28 09:27:45 +0800

    carthage support

---
+++ JXSegmentedViewLib/JXSegmentedViewLib.h
@@ -0,0 +1,19 @@
+//
+//  JXSegmentedViewLib.h
+//  JXSegmentedViewLib
+//
+//  Created by prm on 2019/3/28.
+//  Copyright © 2019 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JXSegmentedViewLib.
+FOUNDATION_EXPORT double JXSegmentedViewLibVersionNumber;
+
+//! Project version string for JXSegmentedViewLib.
+FOUNDATION_EXPORT const unsigned char JXSegmentedViewLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JXSegmentedViewLib/PublicHeader.h>
+
+
